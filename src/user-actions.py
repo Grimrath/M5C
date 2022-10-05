85646955a687f508d6e1330b5db79578a1721f48
Richard Widén <widen.richard@live.se>
2022-10-05 17:24:22 +0200

    initial version

---
+++ src/user-actions.py
@@ -0,0 +1,23 @@
+from add import add
+from view import view
+from menu import menu
+
+
+def user_actions(user, items):
+    print(f"Welcome, {user}!")
+    view("These are your items", items)
+
+    options = {"a":"Add an item", "v":"View items", "l":"Log out"}
+
+    logged_in = True
+    while logged_in:
+        selection = menu("Select an action", "Option: ", options)
+        if selection == "a":
+            add("Add an item: ", items)
+        elif selection == "v":
+            view("These are your items", items)
+        elif selection == "l":
+            logged_in = False
+            return items
+
+    
