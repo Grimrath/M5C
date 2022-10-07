41316f26e1535f5736eade383a08481240a1b7e5
Richard Widén <widen.richard@live.se>
2022-10-07 12:56:08 +0200

    remove sideeffects (print) according to specification

--- src/login.py
+++ src/login.py
@@ -6,12 +6,8 @@ def login(users):
         username = input("    User: ")
         password = input("Password: ")
         if username in users and users[username] == password:
-            # A newline for nice formatting's sake.
-            print()
             return username
         else:
             options = {"r":"Try again", "q":"Quit"}
             if menu("Invalid username or password", "Option: ", options) == "q":
-                # A newline for nice formatting's sake.
-                print()
                 return None
